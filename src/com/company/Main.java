3d9cd27c4fcace34469ee4abb1e98c22463645be
Johannes Schäfers <info@iconicity.de>
2018-08-12 15:51:11 +0200

    1 g Zeilen

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("gg");
-        System.out.println("gg");
-        System.out.println("gg");
-        System.out.println("sucker");
 
 	// write your code here
     }
